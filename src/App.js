221bc2270b483b07789f63e07efe97eb204fbd21
Joel Klemens <joel.klemens@gmail.com>
2021-11-16 12:43:47 -0800

    Added multi list functionality
    -select between multiple to-do lists
    -render items based on which list is selected
    -update list as new to do items are added

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import TodoList from './TodoList';
 import TodoListList from './TodoListList';
 import {v4 as uuidv4} from 'uuid';
 
+//local storage keys for the to do lists and to do list items 
 const LOCAL_STORAGE_KEY = 'todoListItem'
 const LOCAL_STORAGE_KEY_lists = 'todoListList'
 
 
 function App() {
-  const [selected, setSelected] = useState([])
+  //the list of to do items currently selected
+  const [selected, setSelected] = useState()
+  //list of to do lists
   const [todoListList, setTodoListList] = useState([]) 
+  //list of to do items 
   const [todoList, setTodoList] = useState([])
+  //list of to do items that will display 
+  const [todoListDisplay, setTodoListDisplay] = useState([])
   const todoListNameRef = useRef()
   const todoNameRef = useRef()
 
   //when the app first runs get current list of items from the local storage 
   useEffect(() => {
     const storedTodoListList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_lists))
-    console.log("list list: ",storedTodoListList)
+    console.log("lists list: ",storedTodoListList)
     if(storedTodoListList) setTodoListList(storedTodoListList)
   }, [])
 
   //when there is a change to todoListList re-render the list 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY_lists, JSON.stringify(todoListList))
-    console.log("list list: ",todoListList)
+    console.log("lists list: ",todoListList)
   }, [todoListList])
 
   //when the app first runs get current list of items from the local storage 
@@ -52,26 +58,40 @@ function App() {
   }
 
   //clear a list from list of lists 
-  function clearList(e){
-
+  function deleteList(e){
+    const newTodoListList = todoListList.filter(todoListList =>  todoListList.id !== selected.id)
+    console.log("filtered lists list: ", newTodoListList) 
+    setTodoListList(newTodoListList)
   }
 
   //get the selected list to show items
   const getSelected = (e) => {
     const newTodoListList = [...todoListList]
-    const todoList = newTodoListList.find(todoList => todoList.id === e.target.value)
-    if(todoList) console.log('changed list selection: ',todoList)
-    //here we want to set the select list and then load the to do list items based on matching listID 
-    //when creating a to-do item this listID will be added to it 
+    const list = newTodoListList.find(todoList => todoList.id === e.target.value)
+    //const tempDisplay = todoList.
+    if(list) {
+      console.log('changed list selection: ',list) 
+      //set selected list 
+      setSelected(list)
+      //render the items that have this parent id 
+      const tempDisplay = todoList.filter(todoList => todoList.listId === list.id)
+      console.log('display list: ', tempDisplay)
+      setTodoListDisplay(tempDisplay) 
+    } 
   }
 
   //add the new todo item to the existing list 
   function addTodo(e) {
     const name = todoNameRef.current.value
-    //we don't want to add empty todo item
+    //check if name exists then add item to full todo list 
     if (name === '') return
     setTodoList(prevTodoList => {
-      return [...prevTodoList, {id: uuidv4(), listId: '1', name: name, complete: false}]
+      return [...prevTodoList, {id: uuidv4(), listId: selected.id, name: name, complete: false}]
+    })
+    //we also want to add this to the list that is on display 
+    if (name === '') return
+    setTodoListDisplay(prevTodoListDisplay => {
+      return [...prevTodoListDisplay, {id: uuidv4(), listId: selected.id, name: name, complete: false}]
     })
     //clear out the input field
     todoNameRef.current.value = null 
@@ -86,7 +106,7 @@ function App() {
   //toggle the checkmark next to the todo item 
   function toggleTodo(id) {
     const newTodoList = [...todoList]
-    const todo  = newTodoList.find(todo => todo.id === id)
+    const todo = newTodoList.find(todo => todo.id === id)
     todo.complete = !todo.complete
     setTodoList(newTodoList) 
   }
@@ -99,26 +119,27 @@ function App() {
         <h2>Enter name of a new list</h2>
           <input ref={todoListNameRef} type="text" /> 
           <div className="todo-lists-list-buttons">
-            <button onClick={addList}>Add</button>
-            <button onClick={clearList}>Clear</button>  
+            <button onClick={addList}>Add List</button>  
           </div>
           <div>Select a list to view items</div>
           <div className="todo-list-lists">
-            <select defaultValue="Select a to-do list" onChange={e => getSelected(e)}>
+            <select defaultValue={1} onChange={e => getSelected(e)}>
+              <option value={1}>Select a to-do list</option> 
               <TodoListList todoListList={todoListList} /> 
             </select>
+            <button onClick={deleteList}>Delete List</button>
           </div>
         </div>
         <div className="todo-list">
           <h2>Add new item to list</h2>
           <input ref={todoNameRef} type="text" /> 
           <div className="todo-list-buttons">
-            <button onClick={addTodo}>Add</button>
-            <button onClick={clearTodo}>Clear</button>  
+            <button onClick={addTodo}>Add Item</button>
+            <button onClick={clearTodo}>Clear Item</button>  
           </div>
-          <div>Items left to complete {todoList.filter(todo => !todo.complete).length}: </div>
+          <div>Items left to complete {todoListDisplay.filter(todo => !todo.complete).length}: </div>
           <div className="todo-list-items">
-            <TodoList todoList={todoList} toggleTodo={toggleTodo}/> 
+            <TodoList todoList={todoListDisplay} toggleTodo={toggleTodo}/> 
           </div>
         </div>
       </div>
